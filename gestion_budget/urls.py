ca77899c5da85defa2a13254818da3e01b41518e
Cheikh Ahmed Tidiane Gueye <cheikhdev99@gmail.com>
2024-12-19 16:21:19 +0000

    19/12/2024 à 16:20 - Deploiment API

--- gestion_budget/urls.py
+++ gestion_budget/urls.py
@@ -1,4 +1,6 @@
 # gestion_budget/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -7,9 +9,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('budgetsApp.urls')),
-    path('api/utilisateurs/', include('budgetsApp.urls')),
+    # path('api/utilisateurs/', include('budgetsApp.urls')),
     # URLs pour l'authentification google
     path('accounts/', include('allauth.urls')),
 ]
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
